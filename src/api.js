d7f1f258bb2e9094e6fe48212239b3e2c6f56c26
Haci Murat Yaman <haci@muratyaman.co.uk>
2020-05-23 20:46:08 +0100

    #4 db test

--- src/api.js
+++ src/api.js
@@ -1,7 +1,32 @@
+const knex = require('knex');
+
+
+export function dbConfig(){
+  const pe = process.env;
+  const client = pe.DB_CLIENT;
+  let connection = null;
+  if (client === 'mysql') {
+    connection = {
+      host: pe.DB_HOST,
+      user: pe.DB_USER,
+      password: pe.DB_PASSWORD,
+      database: pe.DB_DATABASE,
+    }
+  }
+
+  return {
+    client,
+    connection,
+    pool: { min: 1, max: 5 },
+  };
+}
 
 
 export async function hello() {
-  return Promise.resolve([
-    { name: 'hello from db' },
-  ]);
+  const db = knex(dbConfig());
+  return db.raw('SELECT now() AS ts');
+  //return Promise.resolve([
+  //  { name: 'hello from db' },
+  //]);
 }
+
